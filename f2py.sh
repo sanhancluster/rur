a543968232d51539d25937cd9e8dd7a96370ea70
houseng Choi <hopung@gmail.com>
2020-08-25 13:25:34 +0900

    ensure both steps use the same fortran compiler

--- f2py.sh
+++ f2py.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FILES='readr.f90 readhtm.f90 hilbert3d.f90 io_ramses.f90'
-F2PY=f2py
+F2PY=f2py3
 FORT=gfortran
 BASEDIR=$(dirname "$0")
 
@@ -10,7 +10,7 @@ for f in $FILES
 do
     bn=$(basename "$f" .f90)
     $FORT -x f95-cpp-input -c $f
-    $F2PY -c $f -m $bn --opt='-O3 -x f95-cpp-input'
+    $F2PY -c --f90exec=$FORT $f -m $bn --opt='-O3 -x f95-cpp-input'
 done
 rm *.o *.mod
 
