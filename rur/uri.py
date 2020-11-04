063d0b0dfb929fdd6c93ae5f9185940408eea92a
hansan <sn1994a@gmail.com>
2020-11-04 17:18:14 +0900

    Fix: full_path option

--- rur/uri.py
+++ rur/uri.py
@@ -121,18 +121,19 @@ dtype((numpy.record, [('x', '<f8'), ('y', '<f8'), ('z', '<f8'), ('rho', '<f8'), 
     """
 
     def __init__(self, repo, iout, mode='none', box=None, path_in_repo='', full_path=False, snap=None, longint=False):
+        if(iout<0):
+            output_names = glob.glob(join(self.snap_path, 'output_'+'[0-9]'*5))
+            iouts = [int(arr[-5:]) for arr in output_names]
+            iouts = np.sort(iouts)
+            iout = iouts[iout]
+        self.iout = iout
+
         if(full_path):
             self.path = repo
         else:
             self.repo = repo
             self.path_in_repo = path_in_repo
             self.snap_path = join(repo, path_in_repo)
-            if(iout<0):
-                output_names = glob.glob(join(self.snap_path, 'output_'+'[0-9]'*5))
-                iouts = [int(arr[-5:]) for arr in output_names]
-                iouts = np.sort(iouts)
-                iout = iouts[iout]
-            self.iout = iout
 
             self.path = join(self.snap_path, output_format.format(snap=self))
 
