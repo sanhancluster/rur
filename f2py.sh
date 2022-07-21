1b94d70678a2c69ec96f1bcacd54140ed2a494a1
JinsuRhee <jinsu.rhee@gmail.com>
2022-07-21 10:45:45 +0900

    vrload.py: f2py part edited

--- f2py.sh
+++ f2py.sh
@@ -6,11 +6,22 @@ FORT=gfortran
 BASEDIR=$(dirname "$0")
 
 cd $BASEDIR/rur
+#for f in $FILES
+#do
+#    bn=$(basename "$f" .f90)
+#    $FORT -x f95-cpp-input -c $f
+#    $F2PY -c --f90exec=$FORT --f77exec=$FORT $f -m $bn --opt='-O3 -x f95-cpp-input'
+#done
+#rm *.o *.mod
+
+#For VR part
+cd ..
+FILES='find_domain_py.f90  get_flux_py.f90  get_ptcl_py.f90  jsamr2cell_py.f90  jsamr2cell_totnum_py.f90  js_gasmap_py.f90  js_getpt_ft.f90'
+
+cd $BASEDIR/rur/vr/fortran
 for f in $FILES
 do
     bn=$(basename "$f" .f90)
-    $FORT -x f95-cpp-input -c $f
-    $F2PY -c --f90exec=$FORT --f77exec=$FORT $f -m $bn --opt='-O3 -x f95-cpp-input'
+    $F2PY -m $bn --fcompiler=$FORT --f90flags='-fopenmp' -lgomp -c $f
 done
-rm *.o *.mod
 
