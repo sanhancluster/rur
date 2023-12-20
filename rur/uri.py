259aaf3a05d1b4a80cca20636066200132de906d
Seyoung Jeon <syj3514@yonsei.ac.kr>
2023-12-20 11:22:08 +0900

    update for reading data

--- rur/uri.py
+++ rur/uri.py
@@ -257,6 +257,7 @@ def _classify(pname:str, ids=None, epoch=None, m=None, family=None, sizeonly:boo
                 mask = (ids < 0) & (m > 0) & (epoch == 0)
             nsize = np.count_nonzero(mask)
         elif(ids is not None):
+            print("Warning: either `family` or `epoch` should be given to classify particles.")
             if(pname == 'dm'):
                 mask =  ids > 0
                 nsize = np.count_nonzero(mask)
@@ -315,14 +316,21 @@ def _read_part(fname:str, kwargs:dict, legacy:bool, part=None, mask=None, nsize=
         vx = readorskip_real(f, np.float64, 'vx', target_fields)
         vy = readorskip_real(f, np.float64, 'vy', target_fields)
         vz = readorskip_real(f, np.float64, 'vz', target_fields)
-        m = readorskip_real(f, np.float64, 'm', target_fields)
-        ids = readorskip_int(f, np.int32, 'id', target_fields)
+        if(pname is None):
+            m = readorskip_real(f, np.float64, 'm', target_fields)
+            ids = readorskip_int(f, np.int32, 'id', target_fields)
+        else:
+            m = f.read_reals(np.float64)
+            ids = f.read_ints(np.int32)
         level = readorskip_int(f, np.int32, 'level', target_fields)
         if(isfamily):
             family = f.read_ints(np.int8) # family
             tag = readorskip_int(f, np.int8, 'tag', target_fields) # tag
         if(isstar):
-            epoch = readorskip_real(f, np.float64, 'epoch', target_fields) # epoch
+            if(pname is None):
+                epoch = readorskip_real(f, np.float64, 'epoch', target_fields) # epoch
+            else:
+                epoch = f.read_reals(np.float64)
             metal = readorskip_real(f, np.float64, 'metal', target_fields)
         
         # Masking
@@ -1011,20 +1019,18 @@ dtype((numpy.record, [('x', '<f8'), ('y', '<f8'), ('z', '<f8'), ('rho', '<f8'), 
         if target_fields is not None:
             if( 'cpu' not in target_fields ):
                 target_fields = np.append(target_fields, 'cpu')
-            if( 'epoch' in target_fields ):
-                target_fields = target_fields[target_fields!='epoch']
-            if(pname is not None):
-                # If `pname` is specified, you should include family(or m,epoch) to classify
-                if(isfamily):
-                    if('family' not in target_fields):
-                        target_fields = np.append(target_fields, 'family')
-                else:
-                    if('m' not in target_fields):
-                        target_fields = np.append(target_fields, 'm')
-                    if('epoch' not in target_fields)and(isstar):
-                        target_fields = np.append(target_fields, 'epoch')
-                    if('id' not in target_fields):
-                        target_fields = np.append(target_fields, 'id')
+            # if(pname is not None):
+            #     # If `pname` is specified, you should include family(or m,epoch) to classify
+            #     if(isfamily):
+            #         if('family' not in target_fields):
+            #             target_fields = np.append(target_fields, 'family')
+            #     else:
+            #         if('m' not in target_fields):
+            #             target_fields = np.append(target_fields, 'm')
+            #         if('epoch' not in target_fields)and(isstar):
+            #             target_fields = np.append(target_fields, 'epoch')
+            #         if('id' not in target_fields):
+            #             target_fields = np.append(target_fields, 'id')
             dtype = [idtype for idtype in dtype if idtype[0] in target_fields]
         else:
             target_fields = [idtype[0] for idtype in dtype]
@@ -2167,6 +2173,7 @@ def classify_part(part, pname, ptype=None):
         else:
             mask = False
     elif('id' in names):
+        print("Warning: No `family` or `epoch` field found, using id and mass instead.")
         # DM-only simulation
         if(pname == 'dm'):
             mask =  part['id'] > 0
