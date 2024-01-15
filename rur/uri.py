c2699b15beab81a5b6a65502c40c4777e6dbdcfb
Seyoung Jeon <syj3514@yonsei.ac.kr>
2024-01-15 10:34:44 +0900

    NH2 no D snapshots

--- rur/uri.py
+++ rur/uri.py
@@ -859,6 +859,10 @@ dtype((numpy.record, [('x', '<f8'), ('y', '<f8'), ('z', '<f8'), ('rho', '<f8'), 
         if(self.mode == 'nh2')&(self.iout<60):
             self.hydro_names = hydro_names['y3']
             self.part_dtype = part_dtype['y3']
+        elif(self.mode == 'y4')&(self.iout<60):
+            self.mode = 'y3'
+            self.hydro_names = hydro_names['y3']
+            self.part_dtype = part_dtype['y3']
 
         if(self.classic_format):
             opened.readline()
