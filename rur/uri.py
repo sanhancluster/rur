wrong order for reading fortran

rur/uri.py
@@ -3347,7 +3347,10 @@ def fromndarrays(ndarrays, dtype):
     faster than np.rec.fromarrays
     only works for 2d arrays for now
     """
-    descr = np.dtype(dtype)
+    fdtype = [idt for idt in dtype if 'f' in idt[1]]
+    idtype = [idt for idt in dtype if 'i4' in idt[1]]
+    bdtype = [idt for idt in dtype if 'i1' in idt[1]]
+    descr = np.dtype(fdtype+idtype+bdtype)
 
     itemsize = 0
     nitem = None