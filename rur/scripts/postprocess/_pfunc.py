9d8167cc8795ae8beaa1fb291b8b643e5344c75f
Seyoung Jeon <syj3514@yonsei.ac.kr>
2025-06-19 17:18:13 +0900

    fix postprocess

--- rur/scripts/postprocess/_pfunc.py
+++ rur/scripts/postprocess/_pfunc.py
@@ -11,3 +11,24 @@ def vprint(txt, verbose):
 
 def cprint(txt=""):
     print(f"  |{txt}")
+
+def filemode(fname):
+    import os
+    # import pwd
+    # import grp
+    import stat
+    file_stat = os.stat(fname)
+    # Get owner UID and GID
+    uid = file_stat.st_uid
+    gid = file_stat.st_gid
+
+    # Get username and group name
+    # try:
+    #     user = pwd.getpwuid(uid).pw_name
+    # except:
+    #     user = str(uid)
+    # group = grp.getgrgid(gid).gr_name
+
+    # Get file permission in octal
+    permissions = bool(file_stat.st_mode & stat.S_IWGRP)
+    return uid, gid, permissions
