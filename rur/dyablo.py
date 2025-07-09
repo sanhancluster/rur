ebf37c22a92dffcfe802143b8f9bb4a155dc03e2
San Han <sn1994a@gmail.com>
2025-07-10 00:31:24 +0200

    Improve: amr_projection to automatically select shape

--- rur/dyablo.py
+++ rur/dyablo.py
@@ -3,11 +3,17 @@ import numpy as np
 
 def get_amr(data):
     connectivity = data['connectivity']
+
+    # n_vertices varies with ndim
+    n_vertices = connectivity.shape[1]
+
+    # coordinates are always in 3-dim
     coordinates = np.array(data['coordinates'])
-    vertices = np.reshape(coordinates[connectivity], newshape=(-1, 8, 3))
+    vertices = np.reshape(coordinates[connectivity], newshape=(-1, n_vertices, 3))
     centers = np.mean(vertices, axis=1)
     levels = -np.log2((vertices[:, 1, 0] - vertices[:, 0, 0])).astype(int)
     return centers, levels
+
 
 def amr_projection(data, quantity='rho', shape=100, lims=None, mode='sum', plot_method='cic', projection=['x', 'y'], ndim=3):
     # produce a projection plot of a quantity in the dyablo data
